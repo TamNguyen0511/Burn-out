88e661d64412afc34c642b4fd516089e9d0ad3ea
TamNguyen0511 <96899941+TamNguyen0511@users.noreply.github.com>
2023-11-03 15:35:11 +0700

    feat: basic placement system

---
+++ Assets/_Game/Scripts/Grid/GridData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Game.Scripts.Grid
+{
+    public class GridData
+    {
+        private Dictionary<Vector3Int, PlacementData> _placedObjects = new();
+
+        public void AddObjectAt(Vector3Int gridPosition, Vector2Int objectSize, int ID, int placedObjectIndex)
+        {
+            List<Vector3Int> positionsToOccupy = CalculatePositions(gridPosition, objectSize);
+            PlacementData data = new PlacementData(positionsToOccupy, ID, placedObjectIndex);
+            foreach (var pos in positionsToOccupy)
+            {
+                if (_placedObjects.ContainsKey(pos))
+                    throw new Exception($"Dictionary already contain this cell position{pos}");
+                _placedObjects[pos] = data;
+            }
+        }
+
+        private List<Vector3Int> CalculatePositions(Vector3Int gridPosition, Vector2Int objectSize)
+        {
+            List<Vector3Int> returnValue = new();
+            for (int i = 0; i < objectSize.x; i++)
+            {
+                for (int j = 0; j < objectSize.y; j++)
+                {
+                    returnValue.Add(gridPosition + new Vector3Int(i, 0, j));
+                }
+            }
+
+            return returnValue;
+        }
+
+        public bool CanPlaceObjectAt(Vector3Int gridPosition, Vector2Int objectSize)
+        {
+            List<Vector3Int> positionToOccupy = CalculatePositions(gridPosition, objectSize);
+            foreach (var pos in positionToOccupy)
+            {
+                if (_placedObjects.ContainsKey(pos))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    public class PlacementData
+    {
+        public List<Vector3Int> OccupiedPositions;
+
+        public int ID { get; private set; }
+        public int PlacedObjectIndex { get; private set; }
+
+        public PlacementData(List<Vector3Int> occupiedPositions, int id, int placedObjectIndex)
+        {
+            OccupiedPositions = occupiedPositions;
+            ID = id;
+            PlacedObjectIndex = placedObjectIndex;
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Grid/Placement System/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _Game.Scripts.Grid.Placement_System
+{
+    public class InputManager : MonoBehaviour
+    {
+        public event Action OnClicked, OnExit;
+
+        [SerializeField]
+        private Camera _sceneCamera;
+        [SerializeField]
+        private LayerMask _placementLayerMask;
+
+        private Vector3 _lastPosition;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+                OnClicked?.Invoke();
+            if (Input.GetKeyDown(KeyCode.Escape))
+                OnExit?.Invoke();
+        }
+
+        public bool IsPointerOverUI() => EventSystem.current.IsPointerOverGameObject();
+
+        public Vector3 GetSelectedMapPosition()
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = _sceneCamera.nearClipPlane;
+            Ray ray = _sceneCamera.ScreenPointToRay(mousePos);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, _placementLayerMask))
+            {
+                _lastPosition = hit.point;
+            }
+
+            return _lastPosition;
+        }
+    }
+}
