6aa4bd51ad48404a70a9887ef7f399b018313528
TamNguyen0511 <96899941+TamNguyen0511@users.noreply.github.com>
2023-11-21 10:39:10 +0700

    feat: create IngredientSO.cs

---
+++ Assets/_Game/Scripts/Enums/IngredientState.cs
@@ -0,0 +1,15 @@
+﻿namespace _Game.Scripts.Enums
+{
+    /// <summary>
+    /// This enum show the state of ingredient, which will be passive
+    /// </summary>
+    public enum IngredientState
+    {
+        None,
+        Raw,
+        Cut,
+        Grill,
+        Fry,
+        Steam,
+    }
+}
---
+++ Assets/_Game/Scripts/ScriptableObjects/IngredientSO.cs
@@ -0,0 +1,44 @@
+﻿using _Game.Scripts.Configs.Editors;
+using _Game.Scripts.Enums;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Game.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Ingredient", menuName = "RestaurantGame/Kitchen/Ingredient", order = 0)]
+    public class IngredientSO : ScriptableObject
+    {
+        [SerializeField]
+        private IngredientState _ingredientCurrentState;
+
+        /// <summary>
+        /// Showable Dictionary for this ingredient state and object
+        /// </summary>
+        [SerializeField]
+        private UnitySerializedDictionary<IngredientState, IngredientStateVisualInformation> IngredientStateAndPrefab =
+            new UnitySerializedDictionary<IngredientState, IngredientStateVisualInformation>();
+
+        public void ChangeState(IngredientState newState, GameObject ingredientPrefabVisualObject = null,
+            Image ingredientImageVisual = null)
+        {
+            _ingredientCurrentState = newState;
+
+            if (ingredientPrefabVisualObject != null)
+            {
+                ingredientPrefabVisualObject = IngredientStateAndPrefab[_ingredientCurrentState].IngredientStatePrefab;
+            }
+
+            if (ingredientImageVisual != null)
+            {
+                ingredientImageVisual.sprite = IngredientStateAndPrefab[_ingredientCurrentState].IngredientStateSprite;
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class IngredientStateVisualInformation
+    {
+        public GameObject IngredientStatePrefab;
+        public Sprite IngredientStateSprite;
+    }
+}
